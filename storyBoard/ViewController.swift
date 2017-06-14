b9752755cd46931064895cb25a8bd54492261796
devin-micalisoft <devin.micalisoft@gmail.com>
2017-06-14 13:44:43 -0500

    storyboard tabs

--- storyBoard/ViewController.swift
+++ storyBoard/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var hola = "hola"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
